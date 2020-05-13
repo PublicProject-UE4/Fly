79421a8980bc1b5fe7612c439fbb6ddcd38a4dbf
Tom <798317108@qq.com>
2020-05-13 15:36:21 +0800

    Fly

---
+++ Source/Fly/FlyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLY_API AFlyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
